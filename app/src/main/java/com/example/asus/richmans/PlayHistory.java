838f9606bb91ea4db0055390c223536c9437ba90
amir <amir.jahan.pc.94@gmail.com>
2018-02-23 23:07:26 +0330

    11:07 2/23/2018

---
+++ app/src/main/java/com/example/asus/richmans/PlayHistory.java
@@ -0,0 +1,12 @@
+package com.example.asus.richmans;
+
+
+public class PlayHistory {
+    String beginPeriodDate;
+    String endPeriodDate;
+    String maxCredit;
+
+    public PlayHistory() {
+
+    }
+}
