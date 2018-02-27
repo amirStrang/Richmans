f578e65673d85a37f75aa20c50c93373ebba9597
Mr.Anonymous <mhrd.nba7@gmail.com>
2018-02-27 15:12:57 +0330

    3:12 2/27/2018

--- app/src/main/java/com/example/asus/richmans/RegisterCodeActivity.java
+++ app/src/main/java/com/example/asus/richmans/RegisterCodeActivity.java
@@ -1,8 +1,10 @@
 package com.example.asus.richmans;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
@@ -22,8 +24,15 @@ public class RegisterCodeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register_code);
 
         init();
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegisterCodeActivity.this, HomePageActivity.class));
+            }
+        });
     }
 
     private void init() {
+        btnRegister = (Button) findViewById(R.id.btn_regiser);
     }
 }
