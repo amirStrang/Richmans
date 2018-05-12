ecfa80cb4c007762d43cc0c2b16af52185ba5369
Mr.Anonymous <mhrd.nba7@gmail.com>
2018-05-12 16:06:59 +0430

    history tab2 fixed

--- app/src/main/java/com/example/asus/richmans/HistoryTab2.java
+++ app/src/main/java/com/example/asus/richmans/HistoryTab2.java
@@ -11,11 +11,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.VolleyLog;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.example.asus.richmans.adapter.HistoryCustomListAdapter;
 import com.example.asus.richmans.app.AppController;
 import com.example.asus.richmans.model.History;
@@ -30,7 +33,9 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class HistoryTab2 extends Fragment {
 
@@ -58,45 +63,111 @@ public class HistoryTab2 extends Fragment {
         return rootView;
     }
 
-    void getHistory(String phn) {
+//    void getHistory(String phn) {
+//
+//        String url = "http://seyyedmahdi.eu-4.evennode.com" + phn;
+//
+//        pDialog = new ProgressDialog(getContext());
+//        pDialog.setMessage("لطفا صبر کنید");
+//        pDialog.show();
+//
+//        JsonArrayRequest productReq = new JsonArrayRequest(url,
+//                new Response.Listener<JSONArray>() {
+//                    @Override
+//                    public void onResponse(JSONArray response) {
+//                        Log.d(TAG, response.toString());
+//                        hidePDialog();
+//                        for (int i = 0; i < response.length(); i++) {
+//                            try {
+//                                JSONObject obj = response.getJSONObject(i);
+//                                History history = new History();
+//                                history.setFrom(obj.get("releaseYear") + "/" + obj.get("rating") + "/" + obj.get("rating"));
+//                                history.setTo(obj.get("releaseYear") + "/" + obj.get("rating") + "/" + obj.get("rating"));
+//                                history.setDay(obj.get("rating") + "");
+//                                historyList.add(history);
+//                            } catch (JSONException e) {
+//                                e.printStackTrace();
+//                            }
+//                        }
+//                        adapter.notifyDataSetChanged();
+//                    }
+//                }, new Response.ErrorListener() {
+//            @Override
+//            public void onErrorResponse(VolleyError error) {
+//                VolleyLog.d(TAG, "Error: " + error.getMessage());
+//                hidePDialog();
+//            }
+//        });
+//
+//        // Adding request to request queue
+//        AppController.getInstance().addToRequestQueue(productReq);
+//
+//    }
 
-        String url = "https://api.androidhive.info/json/movies.json?phn=" + phn;
+    private void getHistory(final String phn) {
+        // Tag used to cancel the request
+        String tag_string_req = "req_get_history";
 
-        pDialog = new ProgressDialog(getContext());
-        pDialog.setMessage("لطفا صبر کنید");
-        pDialog.show();
+        String url = "http://seyyedmahdi.eu-4.evennode.com/gethistory";
 
-        JsonArrayRequest productReq = new JsonArrayRequest(url,
-                new Response.Listener<JSONArray>() {
-                    @Override
-                    public void onResponse(JSONArray response) {
-                        Log.d(TAG, response.toString());
-                        hidePDialog();
-                        for (int i = 0; i < response.length(); i++) {
-                            try {
-                                JSONObject obj = response.getJSONObject(i);
-                                History history = new History();
-                                history.setFrom(obj.get("releaseYear") + "/" + obj.get("rating") + "/" + obj.get("rating"));
-                                history.setTo(obj.get("releaseYear") + "/" + obj.get("rating") + "/" + obj.get("rating"));
-                                history.setDay(obj.get("rating") + "");
-                                historyList.add(history);
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                        adapter.notifyDataSetChanged();
+        StringRequest strReq = new StringRequest(Request.Method.POST,
+                url, new Response.Listener<String>() {
+
+            @Override
+            public void onResponse(String response) {
+                Log.d("tag", "Login Response: " + response.toString());
+
+                try {
+                    JSONObject jObj = new JSONObject(response);
+                    String status = jObj.getString("status");
+                    if (!status.equals("ok")) {
+                        return;
                     }
-                }, new Response.ErrorListener() {
+                    JSONArray obj = jObj.getJSONArray("doreha");
+                    for (int i = 0; i < obj.length(); i++) {
+                        try {
+                            JSONObject tobj = obj.getJSONObject(i);
+                            History history = new History();
+                            history.setFrom(tobj.getString("startTime"));
+                            history.setTo(tobj.getString("endTime"));
+                            history.setDay("1");
+                            historyList.add(history);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    adapter.notifyDataSetChanged();
+
+                } catch (JSONException e) {
+                    // JSON error
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+
             @Override
             public void onErrorResponse(VolleyError error) {
-                VolleyLog.d(TAG, "Error: " + error.getMessage());
-                hidePDialog();
+                Log.e("tag", "Login Error: " + error.getMessage());
             }
-        });
+        }) {
+
+            @Override
+            protected Map<String, String> getParams() {
+                // Posting parameters to login url
+                Map<String, String> params = new HashMap<String, String>();
+
+                params.put("id", phn);
+//                params.put("password", password);
+
+
+                return params;
+            }
+
+        };
 
         // Adding request to request queue
-        AppController.getInstance().addToRequestQueue(productReq);
-
+        AppController.getInstance().addToRequestQueue(strReq, tag_string_req);
     }
 
     @Override
